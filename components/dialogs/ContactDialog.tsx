61c5f45b9c563703992520ce8d71363b5bd94595
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2023-01-09 21:44:24 +0100

    this commit will add modal

--- components/dialogs/ContactDialog.tsx
+++ components/dialogs/ContactDialog.tsx
@@ -1,13 +1,36 @@
-import {forwardRef, Fragment, Ref, RefObject, useImperativeHandle, useState} from 'react'
+import {forwardRef, Fragment, Ref, RefObject, useImperativeHandle, useRef, useState} from 'react'
 import {Dialog, Transition} from '@headlessui/react'
+import {addDoc, collection, getFirestore} from "firebase/firestore";
+import {useFirebaseApp} from "reactfire";
+import {TailSpin} from "react-loader-spinner";
 
 export const ContactDialog = forwardRef(function ContactDialog(props: any, ref: Ref<RefObject<any>>){
     const [show, setShow] = useState(false);
-    const [isSubmitted, setSubmitted] = useState(true);
+    const [isSubmitted, setSubmitted] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const app = useFirebaseApp();
+    const firestore = getFirestore(app);
+
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const messageRef = useRef();
+
+    const submit = ()=>{
+        setIsLoading(true);
+        addDoc(collection(firestore, "messages"), {
+            name: nameRef.current?.value,
+            email: emailRef.current?.value,
+            message: messageRef.current?.value,
+        }).then(() => {
+            setIsLoading(false);
+            setSubmitted(true);
+        });
+    }
 
     const loader = "assets/check2.gif";
 
     const open = ()=>{
+        setSubmitted(false);
         setShow(true);
     }
     useImperativeHandle(ref, () => ({ open }), []);
@@ -41,69 +64,98 @@ export const ContactDialog = forwardRef(function ContactDialog(props: any, ref: 
                         >
                             <Dialog.Panel
                                 className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 w-full  md:max-w-sm sm:p-6">
-                                {isSubmitted ? <div className='flex flex-col justify-center items-center text-xl'>
-                                    <span className='text-3xl' >🥳</span>
-                                    Danke für deine Nachricht!
-                                </div> :<div>
-                                    <span
-                                        className="text-3xl mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                                        👋
-                                    </span>
-                                    <div className="mt-3 text-center sm:mt-5">
-                                        <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                                            Say Hi!
-                                        </Dialog.Title>
-                                        <form action="#" method="POST" className=" mt-6 grid grid-cols-1 gap-y-6">
-                                            <div>
-                                                <label htmlFor="full-name" className="sr-only">
-                                                    Full name
-                                                </label>
-                                                <input
-                                                    type="text"
-                                                    name="full-name"
-                                                    id="full-name"
-                                                    autoComplete="name"
-                                                    className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                                                    placeholder="Full name"
-                                                />
+                                {()=> {
+                                    if (isSubmitted && !isLoading){
+                                        return <div className='flex flex-col justify-center items-center text-xl'>
+                                            <span className='text-3xl' >🥳</span>
+                                            Danke für deine Nachricht!
+                                            <button
+                                                onClick={()=>setShow(false)}
+                                                className="mt-4 w-full inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700"
+                                            >
+                                                Back
+                                            </button>
+                                        </div>
+                                    }else if (!isSubmitted && isLoading){
+                                        return <div className='flex justify-center'>
+                                            <TailSpin
+                                                height="80"
+                                                width="80"
+                                                color="#4fa94d"
+                                                ariaLabel="tail-spin-loading"
+                                                radius="1"
+                                                wrapperStyle={{}}
+                                                wrapperClass="mx-auto"
+                                                visible={true}
+                                            />
+                                        </div>
+
+                                    }else {
+                                        return <div>
+                                            <span
+                                                className="text-3xl mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                                                👋
+                                            </span>
+                                            <div className="mt-3 text-center sm:mt-5">
+                                                <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                                                    Say Hi!
+                                                </Dialog.Title>
+                                                <form action="#" method="POST" className=" mt-6 grid grid-cols-1 gap-y-6">
+                                                    <div>
+                                                        <label htmlFor="full-name" className="sr-only">
+                                                            Full name
+                                                        </label>
+                                                        <input
+                                                            type="text"
+                                                            name="full-name"
+                                                            id="full-name"
+                                                            autoComplete="name"
+                                                            className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                                                            placeholder="Full name"
+                                                            ref={nameRef}
+                                                        />
+                                                    </div>
+                                                    <div>
+                                                        <label htmlFor="email" className="sr-only">
+                                                            Email
+                                                        </label>
+                                                        <input
+                                                            id="email"
+                                                            name="email"
+                                                            type="email"
+                                                            autoComplete="email"
+                                                            className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                                                            placeholder="Email"
+                                                            ref={emailRef}
+                                                        />
+                                                    </div>
+                                                    <div>
+                                                        <label htmlFor="message" className="sr-only">
+                                                            Message
+                                                        </label>
+                                                        <textarea
+                                                            id="message"
+                                                            name="message"
+                                                            ref={messageRef}
+                                                            rows={4}
+                                                            className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                                                            placeholder="Message"
+                                                            defaultValue={''}
+                                                        />
+                                                    </div>
+                                                    <div className="mt-5 sm:mt-6">
+                                                        <button onClick={submit}
+                                                            className="w-full inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700"
+                                                        >
+                                                            Send
+                                                        </button>
+                                                    </div>
+                                                </form>
                                             </div>
-                                            <div>
-                                                <label htmlFor="email" className="sr-only">
-                                                    Email
-                                                </label>
-                                                <input
-                                                    id="email"
-                                                    name="email"
-                                                    type="email"
-                                                    autoComplete="email"
-                                                    className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                                                    placeholder="Email"
-                                                />
-                                            </div>
-                                            <div>
-                                                <label htmlFor="message" className="sr-only">
-                                                    Message
-                                                </label>
-                                                <textarea
-                                                    id="message"
-                                                    name="message"
-                                                    rows={4}
-                                                    className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                                                    placeholder="Message"
-                                                    defaultValue={''}
-                                                />
-                                            </div>
-                                            <div className="mt-5 sm:mt-6">
-                                                <button
-                                                    onClick={() => ref.current.open()}
-                                                    className="w-full inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700"
-                                                >
-                                                    Send
-                                                </button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>}
+                                        </div>
+                                    }
+                                }
+                                }
                             </Dialog.Panel>
                         </Transition.Child>
                     </div>
