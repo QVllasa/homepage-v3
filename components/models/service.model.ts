99f0098696e3a1b2c74010d11bfe8616df485e7f
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2023-01-23 16:32:47 +0100

    this commit will add project images

---
+++ components/models/service.model.ts
@@ -0,0 +1,16 @@
+export interface ServiceModel {
+
+    order: number,
+    img: string,
+    slug: string,
+    title: string,
+    sections: {
+        description: string,
+        img: string,
+        keys: { title: string, description: string }[],
+        title: string,
+    }[],
+    key: string[],
+    description: string,
+    pageTitle: string
+}
