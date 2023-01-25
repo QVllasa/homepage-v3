b94ffa5b333fe589748050379fd0e7ed99c9ca83
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2023-01-25 21:59:54 +0100

    this commit will change Q to q

--- components/icons/QComponent.tsx
+++ components/icons/QComponent.tsx
@@ -5,8 +5,9 @@ export const QComponent = () => {
         <div className="flex justify-start lg:w-0 lg:flex-1">
             <Link href={'/'}
                   className={'font-bold transition text-blue-600 dark:text-yellow-500 text-6xl cursor-pointer'}>
-                Q
+                q
                 <span className='text-yellow-400 transition dark:text-blue-600'>.</span>
+
             </Link>
         </div>
     </div>)
