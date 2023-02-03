c85a0f341f0b9bbb2ed349a3165730ef43315d73
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2023-02-03 16:44:25 +0100

    this commit will remove firestore

--- pages/services/[key]/index.tsx
+++ pages/services/[key]/index.tsx
@@ -2,11 +2,176 @@ import {CheckCircleIcon,} from '@heroicons/react/24/outline'
 import {useRouter} from "next/router";
 import {ServiceModel} from "../../../components/models/service.model";
 import Image from "next/image";
-import {Services} from "../../../data/services";
 import {useEffect, useState} from "react";
 
 
-const services = Services;
+const services: ServiceModel[] = [
+    {
+        "title": "IT Consulting for Modern Businesses",
+        "slug": "Optimize you process.",
+        "description": "Unlock your full potential with my IT consulting services. Let me analyze your processes and recommend the best software and tools to optimize them and give you more time for the important things. Achieve maximum efficiency and productivity with my help.",
+        "key": "consulting",
+        "sections": [
+            {
+                "keys": [
+                    {
+                        "description": "Our web development consultation team is composed of experienced developers who have a deep understanding of the latest technologies and trends.",
+                        "title": "Expertise"
+                    },
+                    {
+                        "title": "Customization",
+                        "description": "We provide customized solutions to meet the specific needs of your business, ensuring that your website is tailored to your unique requirements."
+                    },
+                    {
+                        "title": "Results-Driven",
+                        "description": "Our consultation services are designed to help your business achieve its goals, whether it's increasing online sales, boosting website traffic, or improving customer engagement."
+                    }
+                ],
+                "description": "I am a web developer with experience in developing and evaluating third-party web applications. I use this expertise to help people and organizations choose the best web apps for their specific needs. Let me guide you to make informed decisions and achieve your objectives.",
+                "img": "/services/wac.svg",
+                "title": "Web Application Consulting"
+            },
+            {
+                "description": "I help demystify IT and show people that it can be fun and exciting. My experience and guidance can help you see the opportunities IT provides and change your perception of it.",
+                "title": "Teaching",
+                "img": "/services/teaching.svg",
+                "keys": [
+                    {
+                        "description": "Our IT consulting team provides training and education services to help organizations and individuals acquire the skills and knowledge they need to succeed in the ever-changing technology landscape.",
+                        "title": "Knowledge Transfer"
+                    },
+                    {
+                        "description": "We offer customized training programs that are tailored to the specific needs of the organization or individual, ensuring that the content is relevant, engaging and effective.",
+                        "title": "Tailored Content"
+                    },
+                    {
+                        "title": "Proven Results",
+                        "description": "Our training and consulting services have been proven to help organizations and individuals improve their performance, increase productivity, and achieve their goals."
+                    }
+                ]
+            },
+            {
+                "keys": [
+                    {
+                        "description": "Our consulting team has extensive experience in developing and implementing desktop applications for various industries, giving us a deep understanding of the best practices and latest technologies in the field.",
+                        "title": "Experience"
+                    },
+                    {
+                        "title": "Scalability",
+                        "description": "We offer consulting services that can be tailored to the specific needs of your organization, whether you are looking to develop a single application or a suite of applications."
+                    },
+                    {
+                        "description": "Our desktop application consulting services are designed to help organizations increase productivity, streamline processes, and improve overall performance by leveraging the latest technologies and best practices in the field.",
+                        "title": "Productivity Boost"
+                    }
+                ],
+                "img": "/services/desktop.svg",
+                "title": "Desktop Application Consulting",
+                "description": "I use my experience in developing and using various desktop applications to help people and organizations choose the best one for their needs. From simple management tools to complex construction software, I have the expertise to guide you to the right solution. Let me help you make the best use of desktop technologies."
+            }
+        ],
+        "order": 0,
+        "img": ""
+    },
+    {
+        "key": "development",
+        "slug": "Development of tailor-made software.",
+        "pageTitle": "I solve your problem and help you grow.",
+        "order": 1,
+        "description": "Stay ahead of the curve with my cutting-edge technology consulting services. I use state-of-the-art technologies and best practice workflows to deliver fast, high-quality products with consulting and support. Trust me to optimize your workflow, improve performance, and increase efficiency.",
+        "img": "",
+        "sections": [
+            {
+                "img": "/services/websites.svg",
+                "keys": [
+                    {
+                        "title": "Increased Productivity",
+                        "description": "Web applications can streamline processes and automate repetitive tasks, leading to increased productivity and efficiency for businesses."
+                    },
+                    {
+                        "title": "Cost-Effective",
+                        "description": "Developing a web application is generally more cost-effective than traditional software development, as it can be accessed from any device with an internet connection, eliminating the need for costly hardware and software updates."
+                    },
+                    {
+                        "title": "Global Reach",
+                        "description": "Web applications allow businesses to reach a global audience, providing them with the opportunity to expand their customer base and increase their revenue."
+                    }
+                ],
+                "description": "I am a web developer who specializes in creating modern websites for companies to establish a strong online presence and enhance user experience. I also help companies refresh their web presence by redesigning and optimizing their website.",
+                "title": "Websites"
+            },
+            {
+                "keys": [
+                    {
+                        "title": "Scalability",
+                        "description": "Web applications can easily handle an increasing number of users and a growing amount of data, making them a suitable option for businesses of any size."
+                    },
+                    {
+                        "title": "Cost-effectiveness",
+                        "description": "Web applications often have lower development and maintenance costs compared to traditional software."
+                    },
+                    {
+                        "title": "Accessibility",
+                        "description": "Web applications can be accessed from any device with an internet connection, making them easily accessible to customers and employees remotely."
+                    }
+                ],
+                "description": "I can build a tailored, scalable, collaborative and high-performing web application that solves your problems and optimizes your workflows. I work closely with you and your team to understand your specific business needs and will deliver a high-quality, robust and reliable web application that meets your needs. Contact me to build a web application that leaves no desires open.",
+                "title": "Web Application",
+                "img": "/services/apps.svg"
+            }
+        ],
+        "title": "Creating Innovative Solutions for Your Business"
+    },
+    {
+        "order": 2,
+        "description": "Unlock the power of data with my PowerBI and Tableau consulting services. Gain real-time data insights and make data-driven decisions that will give you a competitive edge and help you achieve your goals.",
+        "pageTitle": "I show you things you never knew before.",
+        "img": "",
+        "key": "analytics",
+        "sections": [
+            {
+                "description": "I believe in using my skills and interests to turn problems into opportunities, a mindset that allows me to help others not only find solutions to their problems but also see how they can use those problems to achieve greater things. I work with the motto \"Always turn a problem into an opportunity\" to empower others to see challenges as a means of growth and success. My ability to think creatively and see potential where others might not, helps me to assist others in reaching their goals and realizing their full potential.",
+                "img": "/services/br.svg",
+                "keys": [
+                    {
+                        "description": "Understanding the demographics of your target audience, such as age, gender, income, and education level, allows you to create targeted marketing campaigns that will be more effective in reaching and resonating with your target market.",
+                        "title": "Target audience demographics"
+                    },
+                    {
+                        "description": "Keeping up with industry trends and changes allows you to anticipate customer needs and adapt your marketing strategy accordingly, giving you a competitive edge and making your marketing efforts more effective.",
+                        "title": "Industry trends"
+                    },
+                    {
+                        "title": "Customer feedback",
+                        "description": "Gathering customer feedback through surveys, focus groups, and other research methods allows you to understand customer satisfaction, identify areas for improvement, and measure the effectiveness of your marketing campaigns. This information can be used to make adjustments to your marketing strategy and improve customer retention."
+                    }
+                ],
+                "title": "Busines Research"
+            },
+            {
+                "description": "I use data analytics tools such as PowerBI and Tableau to visualize trends and provide clients with valuable insights into their business. The ability to uncover unexpected trends and insights through data analysis fascinates me and I am passionate about using data to help businesses make better decisions and succeed.",
+                "keys": [
+                    {
+                        "title": "Data-driven decision making",
+                        "description": "Business analytics provides marketers with the ability to make data-driven decisions, by analyzing data from various sources such as customer demographics, website traffic, and sales figures. This allows them to identify key trends, patterns and customer behaviors, providing insights that inform their marketing strategy, and improve ROI."
+                    },
+                    {
+                        "title": "Marketing performance measurement",
+                        "description": "Business analytics enables marketers to measure the performance of their marketing campaigns, by tracking metrics such as website traffic, lead generation, conversion rates and revenue. This helps them to identify which campaigns are working well and which need to be improved."
+                    },
+                    {
+                        "title": "Segmentation and personalization",
+                        "description": "Business analytics enables marketers to segment their customer base and tailor their marketing efforts to different groups. By analyzing data on customer demographics, behavior and purchase history, marketers can create personalized messages and offers that are more likely to resonate with their target audience, resulting in higher conversion rates and customer retention."
+                    }
+                ],
+                "title": "Business Analytics",
+                "img": "/services/ba.svg"
+            }
+        ],
+        "title": "Unlock the Power of Analytics for Your Business",
+        "slug": "Insight about your data."
+    }
+];
 
 export default function Index() {
     const [data, setData] = useState<ServiceModel>()
